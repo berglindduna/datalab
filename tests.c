2bf8ba892736252eb770ff8ea3c180269d5116c8
Berglind D. Sigurdardottir <berglinds14@ru.is>
2016-10-07 18:10:39 +0000

    Create tests.c

---
+++ tests.c
@@ -0,0 +1,150 @@
+/* Testing Code */
+
+#include <limits.h>
+#include <math.h>
+
+/* Routines used by floation point test code */
+
+/* Convert from bit level representation to floating point number */
+float u2f(unsigned u) {
+  union {
+    unsigned u;
+    float f;
+  } a;
+  a.u = u;
+  return a.f;
+}
+
+/* Convert from floating point number to bit-level representation */
+unsigned f2u(float f) {
+  union {
+    unsigned u;
+    float f;
+  } a;
+  a.f = f;
+  return a.u;
+}
+
+/* Copyright (C) 1991-2012 Free Software Foundation, Inc.
+   This file is part of the GNU C Library.
+   The GNU C Library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.
+   The GNU C Library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+   You should have received a copy of the GNU Lesser General Public
+   License along with the GNU C Library; if not, see
+   <http://www.gnu.org/licenses/>.  */
+/* This header is separate from features.h so that the compiler can
+   include it implicitly at the start of every compilation.  It must
+   not itself include <features.h> or any other header that includes
+   <features.h> because the implicit include comes before any feature
+   test macros that may be defined in a source file before it first
+   explicitly includes a system header.  GCC knows the name of this
+   header in order to preinclude it.  */
+/* We do support the IEC 559 math functionality, real and complex.  */
+/* wchar_t uses ISO/IEC 10646 (2nd ed., published 2011-03-15) /
+   Unicode 6.0.  */
+/* We do not support C11 <threads.h>.  */
+int test_negate(int x) {
+  return -x;
+}
+int test_isEqual(int x, int y)
+{
+  return x == y;
+}
+int test_isTmax(int x) {
+    return x == 0x7FFFFFFF;
+}
+int test_bitOr(int x, int y)
+{
+  return x|y;
+}
+int test_getByte(int x, int n)
+{
+    unsigned char byte;
+    switch(n) {
+    case 0:
+      byte = x;
+      break;
+    case 1:
+      byte = x >> 8;
+      break;
+    case 2:
+      byte = x >> 16;
+      break;
+    default:
+      byte = x >> 24;
+      break;
+    }
+    return (int) (unsigned) byte;
+}
+int test_allOddBits(int x) {
+  int i;
+  for (i = 1; i < 32; i+=2)
+      if ((x & (1<<i)) == 0)
+   return 0;
+  return 1;
+}
+int test_logicalShift(int x, int n) {
+  unsigned u = (unsigned) x;
+  unsigned shifted = u >> n;
+  return (int) shifted;
+}
+int test_isAsciiDigit(int x) {
+  return (0x30 <= x) && (x <= 0x39);
+}
+int test_minusOne(void) {
+  return -1;
+}
+int test_divpwr2(int x, int n)
+{
+    int p2n = 1<<n;
+    return x/p2n;
+}
+int test_satMul2(int x)
+{
+  if ((x+x)/2 != x)
+    return x < 0 ? 0x80000000 : 0x7FFFFFFF;
+  else
+    return 2*x;
+}
+int test_subOK(int x, int y)
+{
+  long long ldiff = (long long) x - y;
+  return ldiff == (int) ldiff;
+}
+unsigned test_float_neg(unsigned uf) {
+    float f = u2f(uf);
+    float nf = -f;
+    if (isnan(f))
+ return uf;
+    else
+ return f2u(nf);
+}
+unsigned test_float_abs(unsigned uf) {
+  float f = u2f(uf);
+  unsigned unf = f2u(-f);
+  if (isnan(f))
+    return uf;
+  /* An unfortunate hack to get around a limitation of the BDD Checker */
+  if ((int) uf < 0)
+      return unf;
+  else
+      return uf;
+}
+unsigned test_float_half(unsigned uf) {
+  float f = u2f(uf);
+  float hf = 0.5*f;
+  if (isnan(f))
+    return uf;
+  else
+    return f2u(hf);
+}
+int test_bang(int x)
+{
+  return !x;
+}
